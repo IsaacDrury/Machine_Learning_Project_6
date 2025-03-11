28df69aebc8ba5bde50dae6575a74a6ac97cb20a
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-11 14:35:04 -0400

    Major changes
    Imported mlagents package. Added objects to scene. Created a cameras script to switch between cameras. Created several other scripts and folders. Created food and poison prefabs. Several other changes.

---
+++ Assets/Scripts/Cameras.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Cameras : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainCam;
+    [SerializeField]
+    private GameObject[] agentCams;
+    [SerializeField]
+    private GameObject currentCam;
+    [SerializeField]
+    private int index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        agentCams = GameObject.FindGameObjectsWithTag("AgentCamera");
+        foreach (GameObject cam in agentCams)
+        {
+            cam.SetActive(false);
+        }
+    }
+
+    public void userMainCam()
+    {
+        currentCam.SetActive(false);
+        currentCam = mainCam;
+        currentCam.SetActive(true);
+        index = 0;
+    }
+
+    public void useAgentCams()
+    {
+        if (index == agentCams.Length) 
+        {
+            index = 0;
+        }
+        if (agentCams[index] != null)
+        {
+            currentCam.SetActive(false);
+            currentCam = agentCams[index];
+            currentCam.SetActive(true);
+            index += 1;
+        }
+    }
+}
