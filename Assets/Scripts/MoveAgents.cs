279ea8b1312a6beb49a8dd54d1c5ad2c908ee7d2
IsaacDrury <117859115+IsaacDrury@users.noreply.github.com>
2025-03-11 15:56:19 -0400

    Finished item spawning and agent repositioning
    Food and agents will not spawn/reposition colliding with eachother or with obstacles now. Also removed the poison prefab.

--- Assets/Scripts/MoveAgents.cs
+++ Assets/Scripts/MoveAgents.cs
@@ -1,16 +1,37 @@
+using Grpc.Core;
 using UnityEngine;
 
 public class MoveAgents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject[] agents;
+    [SerializeField]
+    private GameObject[] walls;
 
-    // Update is called once per frame
-    void Update()
+    public void Reposition()
     {
-        
+        foreach (GameObject agent in agents) 
+        {
+            bool goodPos = false;
+            int x = Random.Range(-9, 10);
+            int z = Random.Range(-9, 10);
+            while (!goodPos)
+            {
+                goodPos = true;
+                foreach (GameObject wall in walls)
+                {
+                    Vector3 pos = wall.transform.position;
+                    if (x > pos.x - 1.0f && x < pos.x + 1.0f && z > pos.z - 1.0f && z < pos.z + 1.0f)
+                    {
+                        goodPos = false;
+                        x = Random.Range(-9, 10);
+                        z = Random.Range(-9, 10);
+                        break;
+                    }
+                }
+            }
+            agent.transform.localPosition = new Vector3(x, 0.5f, z);
+        }
+        this.gameObject.GetComponent<SpawnItems>().SpawnFood();
     }
 }
--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -1,4 +1,6 @@
 using System.Data;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnItems : MonoBehaviour
@@ -6,16 +8,13 @@ public class SpawnItems : MonoBehaviour
     [SerializeField]
     private GameObject foodPrefab;
     [SerializeField]
-    private GameObject poisonPrefab;
+    private GameObject[] agents;
     [SerializeField]
     private GameObject[] foods;
+    [SerializeField] 
+    private GameObject[] walls;
     [SerializeField]
     private int foodAmount;
-
-    private void Awake()
-    {
-        foods = new GameObject[foodAmount];
-    }
 
     public void SpawnFood()
     {
@@ -25,11 +24,41 @@ public class SpawnItems : MonoBehaviour
         }
         for (int i = 0; i < foodAmount; i++)
         {
-            float x = Random.Range(-9.0f, 9.0f);
-            float z = Random.Range(-9.0f, 9.0f);
+            bool goodPos = false;
+            int x = Random.Range(-9, 10);
+            int z = Random.Range(-9, 10);
+            while (!goodPos)
+            {
+                goodPos = true;
+                foreach (GameObject agent in agents)
+                {
+                    Vector3 pos = agent.transform.position;
+                    if (x > pos.x - 1.0f && x < pos.x + 1.0f && z > pos.z - 1.0f && z < pos.z + 1.0f)
+                    {
+                        goodPos = false;
+                        x = Random.Range(-9, 10);
+                        z = Random.Range(-9, 10);
+                        break;
+                    }
+                }
+                if (goodPos)
+                {
+                    foreach (GameObject wall in walls)
+                    {
+                        Vector3 pos = wall.transform.position;
+                        if (x > pos.x - 0.5f && x < pos.x + 0.5f && z > pos.z - 0.5f && z < pos.z + 0.5f)
+                        {
+                            goodPos = false;
+                            x = Random.Range(-9, 10);
+                            z = Random.Range(-9, 10);
+                            break;
+                        }
+                    }
+                }
+            }
             GameObject food = Instantiate(foodPrefab);
             food.transform.localPosition = new Vector3(x, 0.5f, z);
-            foods[i] = food;
+            foods.Append(food);
         }
     }
 }
